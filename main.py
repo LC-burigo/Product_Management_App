cfd05753a285d830a0c848b01ff28ae907af2a03
LC-burigo <burigo-jm@hotmail.com>
2023-10-18 13:25:47 -0300

    Working in toolbar function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
 
 class Main(QMainWindow):
     def __init__(self):
@@ -18,8 +19,20 @@ class Main(QMainWindow):
 
     def toolBar(self):
         self.tb=self.addToolBar("Tool Bar")
+        self.tb.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        #####################Toolbar Buttons########################
+        #####################Add Product############################
         self.addProduct = QAction(QIcon('icons/add.png'), "Add Product", self)
         self.tb.addAction(self.addProduct)
+        self.tb.addSeparator()
+        ###########################add Member######################
+        self.addMember = QAction(QIcon('icons/users.png'), "Add Member", self)
+        self.tb.addAction(self.addMember)
+        self.tb.addSeparator()
+        ###########################################################
+        self.sellProduct = QAction(QIcon('icons/sell.png'), "Sell Product", self)
+        self.tb.addAction(self.sellProduct)
+        self.tb.addSeparator()
 
 def main():
     App=QApplication(sys.argv)
