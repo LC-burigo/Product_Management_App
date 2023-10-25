04d76ad33b7371e19ba22ce878b5817e035420d2
LC-burigo <burigo-jm@hotmail.com>
2023-10-25 16:35:11 -0300

    display products

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ class Main(QMainWindow):
         self.tabWigdet()
         self.widgets()
         self.layouts()
+        self.displayProducts()
+        self.displayMembers()
 
     def toolBar(self):
         self.tb = self.addToolBar("Tool Bar")
@@ -66,6 +68,9 @@ class Main(QMainWindow):
         self.productsTable.setHorizontalHeaderItem(3, QTableWidgetItem("Price"))
         self.productsTable.setHorizontalHeaderItem(4, QTableWidgetItem("Quota"))
         self.productsTable.setHorizontalHeaderItem(5, QTableWidgetItem("Availbility"))
+        self.productsTable.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+        self.productsTable.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
+        self.productsTable.doubleClicked.connect(self.selectedProduct)
         #########################right top layout widget#############
         self.searchText = QLabel("Search")
         self.searchEntry = QLineEdit()
@@ -77,12 +82,12 @@ class Main(QMainWindow):
         self.notAvailablealProducts = QRadioButton(" Not Available Products")
         self.listButton = QPushButton("List")
         #########################tab2 widgets#######################
-        self.membersTableWidgets = QTableWidget()
-        self.membersTableWidgets.setColumnCount(4)
-        self.membersTableWidgets.setHorizontalHeaderItem(0, QTableWidgetItem("Member ID"))
-        self.membersTableWidgets.setHorizontalHeaderItem(1, QTableWidgetItem("Member Name"))
-        self.membersTableWidgets.setHorizontalHeaderItem(2, QTableWidgetItem("Member Surname"))
-        self.membersTableWidgets.setHorizontalHeaderItem(3, QTableWidgetItem("Phone"))
+        self.membersTable = QTableWidget()
+        self.membersTable.setColumnCount(4)
+        self.membersTable.setHorizontalHeaderItem(0, QTableWidgetItem("Member ID"))
+        self.membersTable.setHorizontalHeaderItem(1, QTableWidgetItem("Member Name"))
+        self.membersTable.setHorizontalHeaderItem(2, QTableWidgetItem("Member Surname"))
+        self.membersTable.setHorizontalHeaderItem(3, QTableWidgetItem("Phone"))
         self.memberSearchText = QLabel("Search Members")
         self.memberSearchEntry = QLineEdit()
         self.memberSearchButton = QPushButton("Search")
@@ -128,7 +133,7 @@ class Main(QMainWindow):
         self.memberRightLayout.addWidget(self.memberSearchButton)
         self.memberRightGroupBox.setLayout(self.memberRightLayout)
 
-        self.memberLeftLayout.addWidget(self.membersTableWidgets)
+        self.memberLeftLayout.addWidget(self.membersTable)
         self.memberMainLayout.addLayout(self.memberLeftLayout, 70)
         self.memberMainLayout.addWidget(self.memberRightGroupBox, 30)
         self.tab2.setLayout(self.memberMainLayout)
@@ -138,6 +143,77 @@ class Main(QMainWindow):
 
     def funcAddMember(self):
         self.newMember = addmember.AddMember()
+
+    def displayProducts(self):
+        self.productsTable.setFont(QFont("Times", 12))
+        for i in reversed(range(self.productsTable.rowCount())):
+            self.productsTable.removeRow(i)
+
+        query = cur.execute("SELECT product_id, product_name, product_manufacterer, product_price, product_price, product_availability FROM products")
+
+        for row_data in query:
+            row_number = self.productsTable.rowCount()
+            self.productsTable.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.productsTable.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+        self.productsTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+    def displayMembers(self):
+        self.membersTable.setFont(QFont("Times", 12))
+        for i in reversed(range(self.membersTable.rowCount())):
+            self.membersTable.removeRow(i)
+
+        query = cur.execute("SELECT * FROM members")
+
+        for row_data in query:
+            row_number = self.membersTable.rowCount()
+            self.membersTable.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.membersTable.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+        self.membersTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+    def selectedProduct(self):
+        global productId
+        listProduct = []
+        for i in range(0, 6):
+            listProduct.append(self.productsTable.item(self.productsTable.currentRow(), i).text())
+        
+        productId = listProduct[0]
+        self.display = displayProduct()
+        self.display.show()
+
+class displayProduct(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Product Details")
+        self.setWindowIcon(QIcon('icons/icon.ico'))
+        self.setGeometry(450, 150, 350, 600)
+        self.setFixedSize(self.size())
+        self.UI()
+        self.show()
+
+    def UI(self):
+        self.product_Details()
+
+    def product_Details(self):
+        global productId
+        query = ("SELECT * FROM products WHERE product_id=?")
+        product = cur.execute(query, (productId,)).fetchone()
+        self.product_Name = product[1]
+        self.product_Manufacterer = product[2]
+        self.product_Price = product[3]
+        self.product_Qouta = product[4]
+        self.productImg = product[5]
+        self.productStatus = product[6]
+
+    def layouts(self):
+        self.mainLayout = QVBoxLayout()
+        self.topLayout = QVBoxLayout()
+        self.bottomLayout = QFormLayout()
+        self.topFrame = QFrame()
+        self.bottomFrame = QFrame()
 
 def main():
     App=QApplication(sys.argv)
